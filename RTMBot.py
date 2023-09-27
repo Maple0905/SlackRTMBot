68d0d6aab922113b9249d8e670d9c32b5d6834eb
Maple0905 <foreverNo1Techi@gmail.com>
2023-09-26 22:08:46 -0400

    complete

--- RTMBot.py
+++ RTMBot.py
@@ -3,10 +3,6 @@ from slack import RTMClient
 from slack import WebClient
 from dotenv import load_dotenv
 
-slack_token = "xoxb-5947520220534-5951529946549-tAKq2FjfBN0wehKo00CvkGvF"
-web_client = WebClient(token=slack_token)
-rtm_client = RTMClient(token=slack_token)
-
 load_dotenv()
 flag = False
 members = {}
@@ -16,6 +12,7 @@ def send_websocket():
     if not flag:
         flag = True
         global members
+        web_client = WebClient(token=os.getenv("SLACK_BOT_TOKEN"))
         users = web_client.users_list()
         for user in users['members']:
             members[user['id']] = user['profile']['display_name']
@@ -31,21 +28,15 @@ def enable_presence_subscription(**payload):
     global members
     username = members[payload['data']['user']] if members[payload['data']['user']] != "" else payload['data']['user']
     print(f"{username} sent message: {payload['data']['text']}")
-    print("channel", payload['data'])
-    web_client.chat_postEphemeral(
+    web_client = WebClient(token=os.getenv("SLACK_BOT_TOKEN"))
+    response = web_client.chat_postEphemeral(
         channel=payload['data']['channel'],
         text="Hello",
         user=payload['data']['user']
     )
 
-# @RTMClient.run_on(event="presence_change")
-# def presence_change(**payload):
-#     send_websocket()
-#     global members
-#     username = members[payload['data']['user']] if members[payload['data']['user']] != "" else payload['data']['user']
-#     print(f"{username}'s presence is changed: {payload['data']}")
-
 try:
+    rtm_client = RTMClient(token=os.getenv("SLACK_BOT_TOKEN"))
     print("Bot is up and running!")
     rtm_client.start()
 
